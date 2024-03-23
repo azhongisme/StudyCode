ec52b9e025784d55568abc466570a9606dce75f9
azhongisme <1322501114@qq.com>
2024-03-24 00:24:38 +0800

    协程1.cpp提交

---
+++ 协程/协程1.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <coroutine>
+
+struct promise {
+  struct promise_type;
+
+  struct iterator {
+    std::coroutine_handle<promise_type>& h;
+    int& operator*() {
+      return h.promise().n;
+    }
+    iterator operator++() {
+      if (!h.done())h.resume();
+      return *this;
+    }
+    bool operator!=(const iterator&)const {
+      return !h.done();
+    }
+  };
+  struct promise_type {
+    int n;
+    promise get_return_object() {
+      return { std::coroutine_handle<promise_type>::from_promise(*this) };
+    }
+    std::suspend_never initial_suspend() noexcept { return {}; }
+    std::suspend_always final_suspend() noexcept { return {}; }
+    std::suspend_always yield_value(int r) { n = r; return {}; }
+    void return_void() {}
+    void unhandled_exception() {}
+  };
+  iterator begin() { return {_h}; }
+  iterator end() { return {_h}; }
+  std::coroutine_handle<promise_type>_h;
+};
+
+promise iota(int value) {
+  std::cout <<"iota\n";
+  while(value) {
+    co_yield value;
+    --value;
+  }
+}
+
+int main() {
+  for (int x : iota(10)) {
+    std::cout<<x<<"\n";
+  }
+}
