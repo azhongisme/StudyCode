0a7540245ac009c5e075b29d25f4fac77080ed6f
azhongisme <1322501114@qq.com>
2024-03-26 18:51:52 +0800

    thread 添加函数join 与 detach

--- 并发编程/a.cpp
+++ 并发编程/a.cpp
@@ -1,6 +1,7 @@
 #include<iostream>
 #include<thread>
 
+#if 0
 void func() {
   std::cout<<"hello world\n";
 }
@@ -9,4 +10,39 @@ int main() {
   //调用func
   std:: thread t(func);
   t.join();
+}
+#endif
+
+#if 0
+
+int main() {
+  std::thread t ([] {
+    std::cout<<"hello world\n";
+  });
+  t.join();
+}
+#endif
+
+#if 0
+void func(std::string str) {
+  std::cout<<str<<'\n';
+}
+
+int main() {
+  std::thread t (func, "hello world");
+  t.join();
+}
+
+#endif
+
+void func(int n) {
+  for (int i = 1; i <= n; i++) {
+    std::cout<<i<<"\n";
+  }
+}
+
+int main() {
+  std::thread t (func, 100);
+  t.detach();//主线程不等待子线程
+  std::cout<<"main\n";
 }
