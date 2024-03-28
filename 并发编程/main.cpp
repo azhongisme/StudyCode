24a6fc8549807f8174a3766862d78886afbd5bc1
azhongisme <1322501114@qq.com>
2024-03-28 22:04:52 +0800

    lock_guard的实现

--- 并发编程/main.cpp
+++ 并发编程/main.cpp
@@ -2,15 +2,37 @@
 #include <thread>
 #include <mutex>
 
+
+template<class Mutex>
+struct lock_guard {
+    explicit lock_guard(Mutex& m) : _m(m)
+    {
+        _m.lock();
+    }
+    lock_guard(const lock_guard&) = delete;
+    lock_guard& operator = (const lock_guard&) = delete;
+
+    lock_guard(Mutex&m, std::adopt_lock_t) : _m(m) {}
+    ~lock_guard() {
+        _m.unlock();
+    }
+private:
+    Mutex& _m;
+};
+
 std::mutex m;
 
 void func() {
     m.lock();
-    std::lock_guard<std::mutex>lc(m, std::adopt_lock);
-    std::cout<<"hello world\n";
+    ::lock_guard<std::mutex>lc(m, std::adopt_lock);
+    std::cout<<"this is func       this is func\n";
 }
 
 int main() {
-    std::thread t(func);
-    t.join();
+    for (int i = 0; i < 10; i++) {
+        std::thread t(func);
+        std::thread t1(func);
+        t1.join();
+        t.join();
+    }
 }
