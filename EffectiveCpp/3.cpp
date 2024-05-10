64f6b2cb61054bc8d82bf13f068c5f8d86c6672d
azhongisme <1322501114@qq.com>
2024-05-10 20:51:19 +0800

    条款

---
+++ EffectiveCpp/3.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <cstring>
+
+class TextBlock {
+public:
+    TextBlock(const std::string str) : text(str) {}
+    const char& operator[] (std::size_t position) const
+    { return text[position]; }
+    //operator[] for const 对象
+    char& operator[] (std::size_t position)
+    { return text[position]; }
+    //operator[] for non-const 对象
+
+private:
+    std::string text;
+};
+
+class CTextBlock {
+public:
+    std::size_t length() const;
+private:
+    char* pText;
+    mutable std::size_t textLength;
+    mutable bool lengthIsvalid;
+};
+
+std::size_t CTextBlock::length() const {
+    if (!lengthIsvalid) {
+        textLength = std::strlen(pText);
+        lengthIsvalid = true;
+    }
+    return textLength;
+}
+
+int main() {
+    TextBlock tb("hello");
+    tb[0] = 'x';
+    std::cout<<tb[0];
+    const TextBlock ctb("hello");
+    // ctb[0] = 'x'; //error
+    std::cout<<ctb[0];
+}
