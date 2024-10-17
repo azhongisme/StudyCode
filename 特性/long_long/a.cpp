010abe6ef4024f14e5dff4a9cff479bfb0ac83cb
azhongisme <1322501114@qq.com>
2024-10-17 08:33:41 +0800

    long long

---
+++ 特性/long_long/a.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include <limits>
+#include <cstdio>
+
+int main() {
+  std::cout << std::numeric_limits<long long>::max() << std::endl;
+  std::cout << std::numeric_limits<long long>::min() << std::endl;
+}
